3ce83b9bd94735f7a16ca537f6c08bd354ac6fdf
Zhiwei Liang <zhiwei.liang@zliang.me>
2026-04-07 13:45:13 -0400

    Simplify trivial code patterns
    Signed-off-by: Zhiwei Liang <zhiwei.liang@zliang.me>

--- sanic/compat.py
+++ sanic/compat.py
@@ -139,7 +139,7 @@ if use_trio:  # pragma: no cover
         return trio.Path(path).stat()
 
     open_async = trio.open_file
-    CancelledErrors = tuple([asyncio.CancelledError, trio.Cancelled])
+    CancelledErrors = (asyncio.CancelledError, trio.Cancelled)
 else:
     if PYPY_IMPLEMENTATION:
         pypy_os_module_patch()
